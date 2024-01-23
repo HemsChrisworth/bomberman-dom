2035d75ceb07bf2404291211e18d1caa6667a7d8
Hems_Chrisworth <kristianmuldre5@gmail.com>
2024-01-23 09:11:41 +0200

    resized sprite sheet

--- frontend/src/js_modules/consts/consts.js
+++ frontend/src/js_modules/consts/consts.js
@@ -1,7 +1,7 @@
 export const PLAYER_NAME_FORM_INPUT = 'playerName',
 CHAT_MESSAGE_FORM_INPUT_NAME = 'chatMessage',
-MAP_TILE_SIZE = 16,
+MAP_TILE_SIZE = 32,
 SPRITESHEET_ROWS = 23,
-SPRITESHEET_COLUMNS = 14,
+SPRITESHEET_COLUMNS = 14, // important value for calculating sprite positions
 BACKGROUND_TILE_Z_INDEX = 2,
 PLAYER_TILE_Z_INDEX = 0
