be2cc2cdc72f72ffa5bf881f2414c8f36805607d
Hems_Chrisworth <kristianmuldre5@gmail.com>
2024-01-23 20:22:03 +0200

    added player velement in class

--- frontend/src/js_modules/models/playersModel.js
+++ frontend/src/js_modules/models/playersModel.js
@@ -1,6 +1,24 @@
+import { VElement } from "../../../../framework/VElement.js"
+
 export class Player { // add all player properties here, for example image, movements etc
     constructor(name) {
+        this.x = 0
+        this.y = 0
         this.name = name
         this.lives = 3
+        this.sprite = "src/assets/images/spritesheets/spritesheet.png";
+        this.vElement = new VElement({
+          tag: "div",
+          attrs: {
+            src: this.sprite,
+            style: `background-image: url(${this.sprite});
+            background-position: ${spriteSheetPosition};
+            left: ${this.x}px;
+            top: ${this.y}px;
+            width: ${MAP_TILE_SIZE}px;
+            height: ${MAP_TILE_SIZE}px;
+            position: absolute;`,
+          },
+        });
     }
 }
