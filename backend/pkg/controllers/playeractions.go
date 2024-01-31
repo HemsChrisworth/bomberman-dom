57b83610f93a54a5a4ff37b2fe69c697e94bef62
Hems_Chrisworth <kristianmuldre5@gmail.com>
2024-01-31 05:14:41 +0200

    fixed action broadcast problem

--- backend/pkg/controllers/playeractions.go
+++ backend/pkg/controllers/playeractions.go
@@ -30,8 +30,8 @@ func ReplyStartGame(app *application.Application) wsconnection.FuncReplyCreator 
 /*
 broadcasts messages to clients connected to the chat
 */
-func ReplyPlayerAction(app *application.Application) wsconnection.FuncReplyCreator {
-	return func(currConnection *wsconnection.UsersConnection, message webmodel.WSMessage) (any, error) {
+func ReplyPlayerAction(app *application.Application) wsconnection.FuncReplier {
+	return func(currConnection *wsconnection.UsersConnection, message webmodel.WSMessage) error {
 		// action, err := parse.PayloadToString(message.Payload)
 		// if err != nil {
 		// 	return nil, currConnection.WSError(fmt.Sprintf("Invalid payload for a player's action: '%s'", message.Payload), err)
@@ -42,11 +42,11 @@ func ReplyPlayerAction(app *application.Application) wsconnection.FuncReplyCreat
 			Action:   message.Payload,
 		}
 
-		/* _, _, err := currConnection.SendMessageToClientRoom(webmodel.PlayerAction, playerAction)
+		_, _, err := currConnection.SendMessageToClientRoom(webmodel.PlayerAction, playerAction)
 		if err != nil {
-			return nil, currConnection.WSError("sending action to client room failed", err)
-		} */
-		return playerAction, nil
+			return currConnection.WSError("sending action to client room failed", err)
+		}
+		return nil
 		//return "sent", err // reply to current user
 	}
 }
